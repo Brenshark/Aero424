55f16d58c18c70ee64b4a88d4a1aa086b7825fb2
Brendan Richard <143755986+Brenshark@users.noreply.github.com>
2023-08-31 17:06:27 -0400

    Add files via upload

---
+++ Aero 424 Name.cpp
@@ -0,0 +1,12 @@
+﻿// Aero 424 Name.cpp : Defines the entry point for the application.
+//
+
+#include "Aero 424 Name.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Brendan Richard" << endl;
+	return 0;
+}
